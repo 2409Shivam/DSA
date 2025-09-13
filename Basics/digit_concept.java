b7c9ec3f4aea1988ace3ad0854df78cbf85c5f3a
Shivam Singh <156272029+2409Shivam@users.noreply.github.com>
2025-09-13 09:58:07 +0000

    13/September/2025

---
+++ Basics/digit_concept.java
@@ -0,0 +1,14 @@
+// BASIC MATHS CONCEPT 
+
+// Extraction of digits: 
+// N = 7789%10 = 9 and 7789/10=778; N=778%10 = 8 and n/10; N=77%10 = 7 and n/10; N=7%10 = 7 and 7/10=0;
+// We need to %10 and /10 loop till N = 0
+
+//PseudoCode:
+// while(N>0){
+//     lastDigit=N%10;
+//     N=N/10;
+// }
+
+
+
